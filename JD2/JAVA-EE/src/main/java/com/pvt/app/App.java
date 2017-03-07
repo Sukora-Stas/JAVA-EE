69e8b6ada37b6294f407c1530da8521ba7e3f49d
Sukora-Stas <stasiksukora@mail.ru>
2017-03-07 16:52:25 +0300

    main

--- JD2/JAVA-EE/src/main/java/com/pvt/app/App.java
+++ JD2/JAVA-EE/src/main/java/com/pvt/app/App.java
@@ -4,10 +4,6 @@ package com.pvt.app;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-
 
     public static boolean isValidSum(int summ) {
         return summ == 50;
