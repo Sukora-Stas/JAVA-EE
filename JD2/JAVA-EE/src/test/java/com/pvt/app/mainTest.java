cc3e70a8c6680d475e3615290211b9c1eaa6722c
Sukora-Stas <stasiksukora@mail.ru>
2017-03-07 17:00:31 +0300

    tests create

---
+++ JD2/JAVA-EE/src/test/java/com/pvt/app/mainTest.java
@@ -0,0 +1,53 @@
+package com.pvt.app;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created by Sukora Stas.
+ */
+public class mainTest {
+    @Test
+    public void testEmptyHex() {
+        String line = "";
+        String res = main.findAllJavaHexNumbers(line);
+        assertTrue(res.length() == 0);
+    }
+
+    @Test
+    public void testOneHex() {
+        String line = "0xAfd";
+        String res = main.findAllJavaHexNumbers(line);
+        assertTrue(res.indexOf(line) >= 0);
+    }
+
+    @Test
+    public void testNoHex() {
+        String line = "0xAfds";
+        String res = main.findAllJavaHexNumbers(line);
+        assertTrue(res.indexOf(line) == -1);
+    }
+
+    @Test
+    public void testSomeHex() {
+        String[] hex = {"0xAB", "0X1111", "0xfffff"};
+        String line = hex[0] + " asd 0AB " + hex[1] + " " + hex[2] + " ABC";
+        String res = main.findAllJavaHexNumbers(line);
+        for (String h: hex) {
+            assertTrue(res.indexOf(h) >= 0);
+        }
+    }
+
+    @Test
+    public void testAllHex() {
+        String[] hex = {"0xAB", "0X1111", "0xfffff"};
+        StringBuilder line = new StringBuilder("");
+        for (String h: hex) line.append(h).append(" ");
+        String res = main.findAllJavaHexNumbers(line.toString());
+        for (String h: hex) {
+            assertTrue(res.indexOf(h) >= 0);
+        }
+    }
+}
